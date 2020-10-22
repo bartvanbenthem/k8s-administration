56b09b782c1ce9ef13f5a4e51630122f8d9e328e
Bart van benthem <bart.vanbenthem@ns.nl>
2020-10-22 17:52:26 +0200

    update k8s-administration

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module github.com/bartvanbenthem/k8s-hostname
+module github.com/bartvanbenthem/k8s-administration
 
 go 1.14
 
 require (
-	k8s.io/apimachinery v0.16.7
-	k8s.io/client-go v0.16.7
+	k8s.io/apimachinery v0.17.0
+	k8s.io/client-go v0.17.0
 )
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # initialize go modules
-go mod init github.com/bartvanbenthem/k8s-hostname
+go mod init github.com/bartvanbenthem/k8s-administration
 # get the correct go-client module
-go get k8s.io/client-go@kubernetes-1.16.7
+go get k8s.io/client-go@kubernetes-1.17.0
